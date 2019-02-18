ed804377fb75ab72c50999c7091e36a9454e71db
Leo <geniusshiun@gmail.com>
2019-02-18 10:07:00 +0800

    Update keyword_possible.py

--- keyword_possible.py
+++ keyword_possible.py
@@ -21,7 +21,6 @@ def loadSW2IDX(SW2Afilepath):#sw2idx_1226test_v5
                 symboleItem[replaceitem] = subkeyword
 
     return {},list(set(allsubkey)),symboleItem
-
 def recursive_subKey(thissubkey,othersubkeylist,inputStr,thisSets,groupSets):
    
     if thissubkey in othersubkeylist:
@@ -52,8 +51,18 @@ def recursive_subKey(thissubkey,othersubkeylist,inputStr,thisSets,groupSets):
     if not inflag:
         #print('no in, thisSets:',thisSets)
         groupSets.append(thisSets)
-        return 'no more'
-   
+        return 'no more'   
+def generatesub(inputStr,allsubkey):
+    
+    allGroup = {}
+    for subkey in allsubkey:
+        #print(subkey)
+        
+        allGroup[subkey] = []
+        thisSets = []
+        recursive_subKey(subkey, allsubkey.copy(), inputStr, thisSets,allGroup[subkey])
+        #print('=================')
+    return allGroup
 def getAllpossible(inputString,allsubkey,symboleItem):
     
     if str(inputString) == 'nan':
@@ -118,18 +127,18 @@ def getAllpossible(inputString,allsubkey,symboleItem):
                 #for kw in possible.split(' '):
                 #    other.split(' ')
                 # print(possible,other,'PK') #debug
-    return rmGoalList
-def generatesub(inputStr,allsubkey):
-    
-    allGroup = {}
-    for subkey in allsubkey:
-        #print(subkey)
-        
-        allGroup[subkey] = []
-        thisSets = []
-        recursive_subKey(subkey, allsubkey.copy(), inputStr, thisSets,allGroup[subkey])
-        #print('=================')
-    return allGroup
+    # if rmGoalList != sorted(rmGoalList, key=len):
+    #     print(rmGoalList,sorted(rmGoalList, key=len))
+    rmGoalList = sorted(rmGoalList, key=len)
+    locationDict = {}
+    try:
+        for item in rmGoalList:
+            locationDict[item] = inputString.index(item[0].replace('+','').replace('-',''))
+    except:
+        print('ininder')
+    locationDictSort = sorted(locationDict.items(), key=lambda k: k[1])
+    rmGoalList = [locationDictSort[i][0] for i in range(len(locationDictSort))]
+    return rmGoalList#rmGoalList
 def loadfile(filepath):
     inputList = []
     with open(filepath,'r',encoding='utf8') as f:
@@ -138,21 +147,73 @@ def loadfile(filepath):
     
     return inputList
 def main():
-    keyworddict,allsubkey,symboleItem = loadSW2IDX('sw2idx_0119v6')#sw2idx_1226test_v5)
+    
+    parser = argparse.ArgumentParser("Script for possible generate")
+    parser.add_argument("--filepath", '-t', type=str, required=True,help='filepath')
+    parser.add_argument("--idxpath", '-idx', type=str, required=True,help='idxpath')
+    args = parser.parse_args()
+    keyworddict,allsubkey,symboleItem = loadSW2IDX(args.idxpath)#'sw2idx_0119v6'
     allsubkey.extend(['臺灣','臺鐵','臺北','臺中','臺南','臺東','台灣','台鐵','台北','台中','台南','台東'])
     allsubkey = list(set(allsubkey))
-    #parser = argparse.ArgumentParser("Script for web crawler")
-    filepath = 'allhumankey'
-    loadfile(filepath)
+
+    filepath = args.filepath#'allhumankey'
+    
     result = ''
     lineCnt = 0
     for inStr in loadfile(filepath):
         allpossibleList = getAllpossible(inStr,allsubkey,symboleItem)
         #print(allpossibleList)
-        for item in allpossibleList:
+        
+        for items in allpossibleList:
             lineCnt+=1
-            result+=str(lineCnt)+' '+inStr+' => '+item.replace(' ',',')+'\n'
-
-    print(result)
+            inStrList = []
+            inStr = inStr.lower()
+            newStr = inStr
+            for item in items.split(' '):
+                if item in inStr:
+                    inStrList.append(item)
+                # elif item.upper() in inStr:
+                #     inStrList.append(item.upper())
+                #     inStr = inStr.replace(item.upper(),item)
+                # elif item.lower() in inStr:
+                #     inStrList.append(item.lower())
+                #     inStr = inStr.replace(item.lower(),item)
+                elif item.replace('+','').replace('-','') in inStr:
+                    inStrList.append(item.replace('+','').replace('-',''))
+                # elif item in inStr.lower():
+                    
+                #     inStr = inStr.lower()
+                #     inStrList.append(item)
+                # newStr = inStr
+            for item in inStrList:
+                start = [m.start() for m in re.finditer(item,inStr)]
+                end = [m.end() for m in re.finditer(item,inStr)]
+                for s, e in zip(start,end):
+                    if s == 0:
+                        newStr = newStr.replace(item, item+' ')
+                    elif e == len(inStr):
+                        newStr = newStr.replace(item, ' '+item)
+                    else:
+                        newStr = newStr.replace(item, ' '+item+' ')
+                #start, end = [m.start(), m.end() for m in re.finditer(item,inStr)][0]
+                #print(start,end)
+            newStr = ' '.join([item for item in newStr.split(' ') if not item == ''])
+            resultList = []
+            for item in newStr.split(' '):
+                if item in symboleItem:
+                    resultList.append(symboleItem[item])
+                else:
+                    resultList.append(item)
+            try:
+                result+=' '.join(resultList)+'\t'+items.replace(' ',',')+'\n'
+            except:
+                print('|'.join(inStrList),inStr,items)
+            
+            #result+=str(lineCnt)+' '+inStr+' => '+items.replace(' ',',')+'\n'
+        
+    #print(result)
+    with open(filepath+'Allpossible2','w',encoding = 'utf8') as f:
+        f.write(result)
 if __name__ =='__main__':
     main()
+    
